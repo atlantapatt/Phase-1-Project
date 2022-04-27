14fb2e860f915093db612f0161aa039fefb6a875
atlantapatt <99867479+atlantapatt@users.noreply.github.com>
2022-04-26 18:16:12 -0700

    Add to API event listener

--- script.js
+++ script.js
@@ -56,7 +56,9 @@ function searchFunc(event) {
 }
 
 apiButton.addEventListener('click', function() {
+    document.getElementById('output').innerHTML = ""
     randomAPI()
+    
 })
 
 function randomAPI() {
@@ -65,43 +67,49 @@ function randomAPI() {
         .then(response => response.json())
         .then(data => [data].map(meals => {
             //  const html = data.meal 
+            const mealsArray = meals.meals[0]
+            
             const html = `
             <h2>
             Meal: ${meals.meals[0].strMeal}
             </h2>
             <img src = ${meals.meals[0].strMealThumb} width ="200" height="200"/>
             <p>
-            ${meals.meals[0].strMeasure1} ${meals.meals[0].strIngredient1} <br>
-            ${meals.meals[0].strMeasure2} ${meals.meals[0].strIngredient2} <br>
-            ${meals.meals[0].strMeasure3} ${meals.meals[0].strIngredient3} <br>
-            ${meals.meals[0].strMeasure4} ${meals.meals[0].strIngredient4} <br>
-            ${meals.meals[0].strMeasure5} ${meals.meals[0].strIngredient5} <br>
-            ${meals.meals[0].strMeasure6} ${meals.meals[0].strIngredient6} <br>
-            ${meals.meals[0].strMeasure7} ${meals.meals[0].strIngredient7} <br>
-            ${meals.meals[0].strMeasure8} ${meals.meals[0].strIngredient8} <br>
-            ${meals.meals[0].strMeasure9} ${meals.meals[0].strIngredient9} <br>
-            ${meals.meals[0].strMeasure10} ${meals.meals[0].strIngredient10} <br>
-            ${meals.meals[0].strMeasure11} ${meals.meals[0].strIngredient11} <br>
-            ${meals.meals[0].strMeasure12} ${meals.meals[0].strIngredient12} <br>
-            ${meals.meals[0].strMeasure13} ${meals.meals[0].strIngredient13} <br>
-            ${meals.meals[0].strMeasure14} ${meals.meals[0].strIngredient14} <br>
-            ${meals.meals[0].strMeasure15} ${meals.meals[0].strIngredient15} <br>
-            ${meals.meals[0].strMeasure16} ${meals.meals[0].strIngredient16} <br>
-            ${meals.meals[0].strMeasure17} ${meals.meals[0].strIngredient17} <br>
-            ${meals.meals[0].strMeasure18} ${meals.meals[0].strIngredient18} <br>
-            ${meals.meals[0].strMeasure19} ${meals.meals[0].strIngredient19} <br>
-            ${meals.meals[0].strMeasure20} ${meals.meals[0].strIngredient20} <br>
+            ${mealsArray.strMeasure1} ${mealsArray.strIngredient1} <br>
+            ${mealsArray.strMeasure2} ${mealsArray.strIngredient2}  <br>
+            ${mealsArray.strMeasure3} ${mealsArray.strIngredient3} <br>
+            ${mealsArray.strMeasure4} ${mealsArray.strIngredient4} <br>
+            ${mealsArray.strMeasure5} ${mealsArray.strIngredient5} <br>
+            ${mealsArray.strMeasure6} ${mealsArray.strIngredient6} <br>
+            ${mealsArray.strMeasure7} ${mealsArray.strIngredient7} <br>
+            ${mealsArray.strMeasure8} ${mealsArray.strIngredient8} <br>
+            ${mealsArray.strMeasure9} ${mealsArray.strIngredient9} <br>
+            ${mealsArray.strMeasure10} ${mealsArray.strIngredient10} <br>
+            ${mealsArray.strMeasure11} ${mealsArray.strIngredient11} <br>
+            ${mealsArray.strMeasure12} ${mealsArray.strIngredient12}  <br>
+            ${mealsArray.strMeasure13} ${mealsArray.strIngredient13} <br>
+            ${mealsArray.strMeasure14} ${mealsArray.strIngredient14} <br>
+            ${mealsArray.strMeasure15} ${mealsArray.strIngredient15} <br>
+            ${mealsArray.strMeasure16} ${mealsArray.strIngredient16} <br>
+            ${mealsArray.strMeasure17} ${mealsArray.strIngredient17} <br>
+            ${mealsArray.strMeasure18} ${mealsArray.strIngredient18} <br>
+            ${mealsArray.strMeasure19} ${mealsArray.strIngredient19} <br>
+            ${mealsArray.strMeasure20} ${mealsArray.strIngredient20} <br>
+            </p>
+            <p text-align="left">
+            ${mealsArray.strInstructions}
             </p>
             `
             function iterateAPI(array) {
-              [array].filter(function (element) {
-                  return element.value !== null
-              })
+    
             }
-            console.log(iterateAPI(meals.meals[0]))
+
+            // console.log(iterateAPI(mealsArray))
+            console.log(mealsArray)
+            console.log()
 
              document.querySelector('#output').insertAdjacentHTML('afterbegin', html)
-             console.log(meals.meals[0])
+            //  console.log(meals.meals[0])
             
     }))
     
